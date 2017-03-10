409c013ef180e919a9c6530235cf8c9c9390410b
LeinadV <leinadv86@gmail.com>
2017-03-11 00:49:14 +0800

    keep on basic structure and design binary decoder (I think there're lots of bugs but I have to sleep)

---
+++ simulator/instruction.cpp
@@ -0,0 +1,44 @@
+#include"single_cycle.h"
+#include<iostream>
+#include<fstream>
+
+char get_bits(char c, int a, int b) //get part of bits in a byte;
+{
+    if(a<b || a>7 || b>7 || a<0 || b<0) return c; //76543210, a is left bound, b is right
+    else{
+        c << (7-a);
+        c >> (7-a+b);
+        return c;
+    }
+}
+
+void decode_iimage(unsigned int &pc, vector<Instruction> &ins_vec)
+{
+    ifstream iimage("iimage.bin", ios::in|ios::binary|ios::ate);
+    char* imem;
+    if(iimage.is_open()){
+        auto size=iimage.tellg();
+        imem=new char [size];
+        iimage.seekg(0, ios::beg);
+        iimage.read(imem, size);
+        iimage.close();
+        
+        pc=0;
+        for(int i=0;i<8;++i){
+            pc+=imem[i];
+            pc<<4;
+        }
+        unsigned int num=0;
+        for(int i=0;i<8;++i){
+            num+=imem[i];
+            num<<4;
+        }
+        int pos=8;
+
+        while(num--){
+            unsigned int opcode=(((unsigned int)(imem[pos])) << 2) + (unsigned int)(get_bits(imem[pos+1], 7, 6));
+            if(opcode==)
+            pos+=4;
+        }
+    }else cout << "Cannot open iimage.bin\n";
+}
--- simulator/single_cycle.h
+++ simulator/single_cycle.h
@@ -1,3 +1,46 @@
+#define add 0x20
+#define addu 0x
+#define sub 0x
+#define and 0x
+#define or 0x
+#define xor 0x
+#define nor 0x
+#define nand 0x
+#define slt 0x
+#define sll 0x
+#define srl 0x
+#define sra 0x
+#define jr 0x
+#define mult 0x
+#define multu 0x
+#define mfhi 0x
+#define mflo 0x
+
+#define addi 0x
+#define addiu 0x
+#define lw 0x
+#define lh 0x
+#define lhu 0x
+#define lb 0x
+#define lbu 0x
+#define sw 0x
+#define sh 0x
+#define sb 0x
+#define lui 0x
+#define andi 0x
+#define ori 0x
+#define nori 0x
+#define slti 0x
+#define beq 0x
+#define bne 0x
+#define bgtz 0x
+
+#define j 0x
+#define jal 0x
+
+#define halt 0x
+
+
 class Instruction{
     public:
         Instruction(): opcode(0), rs(0), rt(0), rd(0), shamt(0), funct(0);
@@ -5,11 +48,12 @@ class Instruction{
         Instruction(int opcode, int rs, int rt, int rd, int shamt, int funct): opcode(opcode), rs(rs), rt(rt), rd(rd), shamt(shamt), funct(funct);
 
     private:
-        int opcode;
-        int rs;
-        int rt;
-        int rd;
-        int shamt;
-        int funct;
+        unsigned int opcode;
+        unsigned int rs;
+        unsigned int rt;
+        unsigned int rd;
+        unsigned int shamt;
+        unsigned int funct;
+        unsigned int addr;
 };
 
