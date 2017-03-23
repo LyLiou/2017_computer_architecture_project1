b6804a5422c4d2ee383e8832bb2e9b0a0c7e7c62
LeinadV <leinadv86@gmail.com>
2017-03-23 21:53:59 +0800

    super powerful makefile

--- simulator/single_cycle.cpp
+++ simulator/single_cycle.cpp
@@ -2,7 +2,6 @@
 #include<iostream>
 #include<vector>
 
-
 uint num_inst, pc;
 std::vector<uint> iimage;
 int main()
@@ -17,15 +16,5 @@ int main()
     //num_inst=get_num_inst(iimage[0]);
     std::cout << "# of inst: " << num_inst << " " << iimage.size() << "\n";
     
-    /*uint pos=8;    
-    for(uint i=0;i<num_inst;++i){
-        //std::string ins_src(imems, pos, 4);
-        std::string ins_src;
-        for(int j=0;j<4;++j) ins_src.push_back(iimage[pos]+j);
-        Instruction ins_cur=decode_iimage(ins_src);
-        std::cout << i << " ";
-        ins_cur.print();
-        pos+=4;
-    }*/
     return 0;
 }
